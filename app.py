8d17d90d7a6ca523e9d249c3f1ce7b2dd1bc02f5
Mayank Panjiyara <mayank.panjiyara2002@gmail.com>
2022-06-07 08:40:22 +0000

    Add app.py and requriments

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Flask App"
+
+@app.route("/<name>")
+def msg(name):
+    return f"Hello {name}"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
